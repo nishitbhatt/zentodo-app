846db5507113441f3792d4cc725323c7b2127849
nishitbhatt <nishit.bhatt96@gmail.com>
2022-10-14 17:20:31 +0530

    Database URI Changed

--- api/app/config/connection.js
+++ api/app/config/connection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
-const mongoUri = 'mongodb://localhost:27017/zentodo';
-// const mongoUri = 'mongodb+srv://TaskManager:Paparocks_123@cluster0.3rcip.mongodb.net/zentodo';
+// const mongoUri = 'mongodb://localhost:27017/zentodo';
+const mongoUri = 'mongodb+srv://TaskManager:Paparocks_123@cluster0.3rcip.mongodb.net/zentodo';
 mongoose.connect(mongoUri).then((data) => {
     console.log('Connction successfull !!');
 }).catch((e) => {
