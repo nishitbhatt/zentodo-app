275994bc22bee3c83a377edd938c222a43a92bea
nishitbhatt <nishit.bhatt96@gmail.com>
2022-10-20 16:21:10 +0530

    Patch Commit

--- api/app/config/connection.js
+++ api/app/config/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
-const LOCAL_MONGO_URI = 'mongodb://localhost:27017/zentodo';
+// const LOCAL_MONGO_URI = 'mongodb://localhost:27017/zentodo';
 const LIVE_MONGO_URI = 'mongodb+srv://TaskManager:Paparocks_123@cluster0.3rcip.mongodb.net/zentodo';
-const mongoUri = LOCAL_MONGO_URI || LIVE_MONGO_URI
+const mongoUri =  LIVE_MONGO_URI
 mongoose.connect(mongoUri).then((data) => {
     console.log('Connction successfull !!', process.env.DATABASESTRING,);
 }).catch((e) => {
